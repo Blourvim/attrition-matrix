161c3da711b3d9b29eb5932cdc3820777b13fa1d
blourvim <blourvim@gmail.com>
2026-03-03 18:22:12 +0300

    fix the database connection issue

--- code/src/main.rs
+++ code/src/main.rs
@@ -2,6 +2,7 @@ use actix_files::Files;
 use actix_web::{App, HttpServer, web};
 use dotenv;
 use product_eng_interview::api::api::api_scope;
+use sea_orm::{ConnectOptions, Database};
 #[actix_web::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     dotenv::dotenv().ok();
@@ -12,9 +13,11 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let intermediate_db_url = std::env::var("INTERMEDIATE_DB_URL").unwrap();
 
     let mut opt = ConnectOptions::new(intermediate_db_url);
-    let int_db = Database::connect(opt).await?;
+    let inter_db = Database::connect(opt)
+        .await
+        .expect("failed to initiate initer_db");
 
-    HttpServer::new(|| {
+    HttpServer::new(move || {
         App::new()
             .service(api_scope())
             .service(
@@ -22,7 +25,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                     .prefer_utf8(true)
                     .index_file("index.html"),
             )
-            .app_data(web::Data::new(int_db))
+            .app_data(web::Data::new(inter_db.clone()))
     })
     .bind(("0.0.0.0", 3001))?
     .run()
